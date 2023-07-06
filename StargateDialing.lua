30b322852c1b756558575af359ffdcdb93623286
Povstalec <64008165+Povstalec@users.noreply.github.com>
2023-07-06 10:44:45 +0200

    Initial commit

---
+++ StargateDialing.lua
@@ -0,0 +1,49 @@
+interface = peripheral.wrap("basic_interface_2")
+
+function dial(address)
+   local addressLength = #address
+   local start = interface.getChevronsEngaged() + 1
+   
+    for chevron = start,addressLength,1
+    do
+    
+        local symbol = address[chevron]
+        
+        if chevron % 2 == 0 then
+            interface.rotateClockwise(symbol)
+        else
+            interface.rotateAntiClockwise(symbol)
+        end
+        
+        while(not interface.isCurrentSymbol(symbol))
+        do
+            print("Feedback",interface.getRecentFeedback())
+            sleep(0)
+        end
+        
+        sleep(1)
+        print("Code",interface.raiseChevron())
+        sleep(1)
+        print("Code",interface.lowerChevron())
+        sleep(1)
+    end 
+end
+
+abydosAddress = {26,6,14,31,11,29,0}
+chulakAddress = {8,1,22,14,36,19,0}
+lanteaAddress = {18,20,1,15,14,7,19,0}
+badAddress = {1,0}
+
+print("Avaiting input:")
+print("1 = Abydos")
+print("2 = Chulak")
+print("3 = Lantea")
+input = tonumber(io.read())
+
+if input == 1 then
+    dial(abydosAddress)
+elseif input == 2 then
+    dial(chulakAddress)
+elseif input == 3 then
+    dial(lanteaAddress)
+end
